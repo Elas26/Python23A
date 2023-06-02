65c9ed4661ef1820fc96dcb4bc97b5b7172e3b29
Elizabeth Arias <fatima.arias3171@alumnos.udg.mx>
2023-06-02 15:44:41 -0600

    Proceso de concatenacion

--- programa2.py
+++ programa2.py
@@ -1,4 +1,11 @@
-preciodemanzanas:int= 10
-cantidaddemanzanas:int= 5
+###INPUT SOLO GUARDA TEXTO, SI SON NUMEROS SE UTILIXA LA FUNCION INT
+preciodemanzanas= int(input("Ingresa el precio: "))
+cantidaddemanzanas= int(input("Ingresa la cantidad: "))
 print("Vas a pagar")
 print(preciodemanzanas * cantidaddemanzanas)
+
+#PROCESO DE CONCATENACION
+
+print("las manzanas estan en: " + str(preciodemanzanas))
+print("y fueron: " ,cantidaddemanzanas)
+print(f"Total a pagar: {preciodemanzanas*cantidaddemanzanas} pesos")
