7530691b37a01ebf2855134d5f65118731efabc7
Elizabeth Arias <fatima.arias3171@alumnos.udg.mx>
2023-06-07 12:51:09 -0600

    .

--- Conversion.py
+++ Conversion.py
@@ -1,6 +1,4 @@
 cantidad_dolares= int(input("Ingresa la cantidad: "))
 tasa_cambio=float(input("Ingresa la tasa de cambio: "))
 peso_mexicano=cantidad_dolares*tasa_cambio
-print(f'{cantidad_dolares} dólares son {peso_mexicano} pesos mexicanos.')
-Pruebo que sirva git
-hola jeje
+print(f"{cantidad_dolares} dolares son: {cantidad_dolares * tasa_cambio} pesos")
