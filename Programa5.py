c3e2399d1b7e91fd88a2ed01cf95c7dc4fe35a4c
Elizabeth Arias <fatima.arias3171@alumnos.udg.mx>
2023-06-09 14:49:04 -0600

    Funcion while

---
+++ Programa5.py
@@ -0,0 +1,24 @@
+# contador = 0   
+# while contador < 5 :
+#     #print ("contador %d" % contador ) 
+#     print("Hola")
+#     #contador= contador + 1 
+#     contador +=1 #Otra opcion de hacer el contador + 1
+    
+# tabla= 5
+# numero = 1
+# while tabla < 51 :
+#     print(f"5 x {numero} = {tabla}")
+#     tabla = tabla + 5
+#     numero = numero + 1
+
+# tabla= int(input("Ingresa el numero de tabla a multiplicar: "))
+# numero_multiplicado = 1
+# while numero_multiplicado <= 100 :
+#     print(f"{tabla} x {numero_multiplicado} = {tabla * numero_multiplicado} ")
+#     numero_multiplicado= numero_multiplicado + 1
+    
+valida = 1
+while valida != 0 :
+        print("Hola")
+        valida = int(input("si quieres salir presiona 0"))
